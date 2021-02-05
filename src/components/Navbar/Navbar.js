3820877c73cbb918a4a7f667c92910a3d1b487ef
Brandon Callegari <bc.speaklife@gmail.com>
2021-02-05 15:24:20 -0600

    GlobalStyles added for btn-Adding btns

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,42 @@
-import React, { useState } from 'react';
-import {IconContext} from 'react-icons/lib';
+import React, { useState, useEffect } from 'react';
+import { IconContext } from 'react-icons/lib';
 import { FaBars, FaTimes } from 'react-icons/fa';
+import { Button } from '../../globalStyles';
 import {
     Nav,
     NavbarContainer,
     NavLogo,
     NavIcon,
-    MobileIcon
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks,
+    NavItemBtn,
+    NavBtnLink,
 } from './Navbar.elements';
 
 
 
 const Navbar = () => {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
 
+    const showButton = () => {
+        if(window.innerwidth <= 960) {
+            setButton(false) 
+        } else {
+            setButton(true)
+        }
+    }
+
+    useEffect(() => {
+        showButton();
+    },[]);
+
+    window.addEventListener('resize', showButton); 
+    
     return (
         <IconContext.Provider value={{ color: '#fff' }}>
             <Nav>
@@ -25,6 +46,18 @@ const Navbar = () => {
                     ULTRA
                 </NavLogo>
                     <MobileIcon onClick={handleClick}> {click ? <FaTimes /> : <FaBars />} </MobileIcon>
+                    <NavMenu onclick={handleClick} click={click}>
+                        <NavItem>
+                            <NavLinks to='/'>Home</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='/services'>Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='/products'>Products</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+
                 </NavbarContainer>
             </Nav>
         </IconContext.Provider>
