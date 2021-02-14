d8762dec682d7045f12bf7262d2d1c124148558f
Brandon Callegari <bc.speaklife@gmail.com>
2021-02-14 17:13:26 -0600

    InfoSection in Progress; visual.

--- src/components/InfoSection/InfoSection .js
+++ src/components/InfoSection/InfoSection .js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { InfoSec } from './InfoSection.element';
 import { Container } from '../../globalStyles';
+import {
+    InfoSec,
+    InfoRow,
+    InfoColumn,
+    TextWrapper
+} from './InfoSection.elements'
 
-const InfoSection = ({ lightBg }) => {
+const InfoSection = ({ lightBg, imgStart }) => {
     return (
     
             <InfoSec lightBg={lightBg}>
                 <Container>
                     <InfoRow imgStart={imgStart}>
                         <InfoColumn>
-                            <TextWrapper>h1</TextWrapper>
+                            <TextWrapper>h</TextWrapper>
                         </InfoColumn>
                     </InfoRow>
                 </Container>
