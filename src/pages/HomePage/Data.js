9ae95f3c8b0d7bc1f46b46ebb71a247b3fb9feb8
Brandon Callegari <bc.speaklife@gmail.com>
2021-02-15 01:23:00 -0600

    Infosection complete, images added; ready for customization

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -13,3 +13,51 @@ export const homeObjOne = {
     alt: 'Image',
     start: ''
 };
+
+export const homeObjTwo = {
+    lightBg: false,
+    primary: true,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'I help business owners increase their revenue. My team of unique specialists can help you achieve your business goals.',
+    headLine: 'Lead Generation Specialist for Online Businesses',
+    lightText: true,
+    topLine: 'Marketing Agency',
+    img: require('../../images/IMG_0668.jpg'),
+    alt: 'Image',
+    start: ''
+};
+
+export const homeObjThree = {
+    lightBg: false,
+    primary: true,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'I help business owners increase their revenue. My team of unique specialists can help you achieve your business goals.',
+    headLine: 'Lead Generation Specialist for Online Businesses',
+    lightText: true,
+    topLine: 'Marketing Agency',
+    img: require('../../images/undraw_black_lives_matter_rndk.svg'),
+    alt: 'Image',
+    start: ''
+};
+
+export const homeObjFour = {
+    lightBg: false,
+    primary: true,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'I help business owners increase their revenue. My team of unique specialists can help you achieve your business goals.',
+    headLine: 'Lead Generation Specialist for Online Businesses',
+    lightText: true,
+    topLine: 'Marketing Agency',
+    img: require('../../images/undraw_develop_app_re_bi4i.svg'),
+    alt: 'Image',
+    start: ''
+};
--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import {homeObjOne} from './Data';
+import {homeObjFour, homeObjOne, homeObjTwo, homeObjThree} from './Data';
 import { InfoSection } from '../../components';
 
 
 const Home = () => {
     return (
-        
+        <React.Fragment>
             <InfoSection  {...homeObjOne} />
-
+            <InfoSection  {...homeObjTwo} />
+            <InfoSection  {...homeObjThree}/>
+            <InfoSection  {...homeObjFour} />
+        </React.Fragment>
     );
 };
 
